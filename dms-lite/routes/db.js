21c3db5ae5b41f69d047cad80e762a1874485ab4
Teikahym <sebifri@gmail.com>
2022-07-24 16:48:20 +0200

    Add functionality to send wifi on/off for specific ducks (frontend only)
    Signed-off-by: Teikahym <sebifri@gmail.com>

--- dms-lite/routes/db.js
+++ dms-lite/routes/db.js
@@ -19,6 +19,7 @@ async function testDbConnection() {
 	try {
 		conn = await pool.getConnection();
 		const createTable = await conn.query("CREATE TABLE IF NOT EXISTS clusterData (timestamp DATETIME, duck_id TEXT, topic TEXT, message_id TEXT, payload TEXT, path TEXT, hops INT, duck_type INT)");
+		const createCommandsTable = await conn.query("CREATE TABLE IF NOT EXISTS clusterCommands (timestamp TEXT, topic TEXT, payload TEXT)");
 		console.log("Checked if table exists");
 		const query = await conn.query("INSERT INTO clusterData(timestamp, duck_Id, topic, message_id, payload, path, hops, duck_type) values ('2021-11-14 00:00:00', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 1, 1);");
 	} catch (err) {
@@ -65,4 +66,10 @@ function getDuckPlusData() {
 	return pool.query(sql).catch(error => console.log(error));
 }
 
-module.exports = { getAllData, getDataByDuckId, getUniqueDucks, getLastCount, getDuckPlusData };
+function postCommand(topic, payload) {
+	const sql = "INSERT INTO clusterCommands VALUES (?,?,?);";
+	pool.query(sql, [new Date().toISOString(),topic,payload]);
+	return true;
+}
+
+module.exports = { getAllData, getDataByDuckId, getUniqueDucks, getLastCount, getDuckPlusData, postCommand };
