c2cda550c30c29ceffa5cff88cf35cdb477d0257
Teikahym <sebifri@gmail.com>
2022-07-25 16:37:24 +0200

    fixed tablefilter include
    Signed-off-by: Teikahym <sebifri@gmail.com>

--- dms-lite/app.js
+++ dms-lite/app.js
@@ -35,7 +35,7 @@ app.use('/ducks', ducksRouter);
 // Serve libraries that were installed via npm - thanks to https://stackoverflow.com/a/70744989
 app.use('/stylesheets/leaflet/', express.static(path.join(__dirname, "node_modules/leaflet/dist/")));
 app.use('/stylesheets/bootstrap/', express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
-app.use('/stylesheets/tablefilter/', express.static(path.join(__dirname, "node_modules/leaflet/dist/")));
+app.use('/stylesheets/tablefilter/', express.static(path.join(__dirname, "node_modules/tablefilter/dist/tablefilter/")));
 
 app.get("/", (req, res) => {
   // check if user is logged in, by checking cookie
